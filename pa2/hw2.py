fae736af760f14d42fd311d33ab6960997b09ac6
masonrware <86129546+masonrware@users.noreply.github.com>
2022-02-01 18:52:19 -0500

    created pa2

---
+++ pa2/hw2.py
@@ -0,0 +1,52 @@
+from pathlib import Path
+from flask import Flask, render_template, request
+from utils import title_match, load_wapo
+
+app = Flask(__name__)
+
+DATA_DIR = Path(__file__).parent.joinpath("pa2_data")
+wapo_path = DATA_DIR.joinpath("wapo_pa2.jl")
+wapo_docs = load_wapo(wapo_path)  # load and process WAPO documents
+
+
+@app.route("/")
+def home():
+    """
+    home page
+    :return:
+    """
+    return render_template("home.html")
+
+
+@app.route("/results", methods=["POST"])
+def results():
+    """
+    result page
+    :return:
+    """
+    query_text = request.form["query"]  # Get the raw user query from home page
+    return render_template("results.html", query=query_text)  # add variables as you wish
+
+
+@app.route("/results/<int:page_id>", methods=["POST"])
+def next_page(page_id):
+    """
+    "next page" to show more results
+    :param page_id:
+    :return:
+    """
+    return render_template("results.html")  # add variables as you wish
+
+
+@app.route("/doc_data/<doc_id>")
+def doc_data(doc_id):
+    """
+    document page
+    :param doc_id:
+    :return:
+    """
+    return render_template("doc.html")  # add variables as you wish
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5000)
---
+++ pa2/utils.py
@@ -0,0 +1,42 @@
+import json
+import os
+from typing import Dict, Union
+
+
+def title_match(query: str, title: str) -> bool:
+    raise NotImplementedError
+
+
+def load_wapo(wapo_jl_path: Union[str, os.PathLike]) -> Dict[str, Dict]:
+    """
+    output dictionary should be of the following format:
+    {
+      "2ee2b1ca-33d9-11e1-a274-61fcdeecc5f5": {
+        "id": "2ee2b1ca-33d9-11e1-a274-61fcdeecc5f5",
+        "title": "Many Iowans still don't know who they will caucus for",
+        "author": "Jason Horowitz",
+        "published_date": 1325380672000,
+        "content_str": "Iran announced a nuclear fuel breakthrough and test-fired ..."
+      },
+      "another id": {...}
+    }
+
+    "content_str" is a new field that you need to generate. The value of "content_str" is the concatenation of
+    content values that are typed as "sanitized_html" from "contents" field.
+
+    """
+    output_dict = dict()
+    with open(wapo_jl_path) as file:
+        for line in file:
+            conv = json.loads(line)
+            contents = conv['contents']
+            content_str_ = " ".join([item['content'] for item in contents if item['type'] == 'sanitized_html'])
+            value_dict = {
+                'id': conv['id'],
+                'title': conv['title'],
+                'author': conv['author'],
+                'published_date': conv['published_date'],
+                'content_str': content_str_
+            }
+            output_dict[conv['id']] = value_dict
+    return output_dict
