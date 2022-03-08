9ab86caba2d0e192f5fd1e9b0fd9bb439fbd08c8
masonrware <86129546+masonrware@users.noreply.github.com>
2022-03-08 12:18:48 -0500

    major updates - working on inverted index at the moment

--- WareMason_PA4/inverted_index.py
+++ WareMason_PA4/inverted_index.py
@@ -1,29 +1,32 @@
-from pydoc import doc
+import re
+import math
 from typing import List, Tuple, Dict, Iterable
-from utils import timer
+from pydoc import doc
 
+from utils import timer
 from text_processing import TextProcessing
-from mongo_db import db, insert_doc_len_index
+from mongo_db import insert_doc_len_index, insert_vs_index, query_doc, query_vs_index
 
 from scipy import spatial
+
+N = 0
 
 text_processor = TextProcessing.from_nltk()
 
 class InvertedIndex:
     """Inverted Index Data Structure to map tokens to their postings."""
-    def __init__(self, n: int):
+    def __init__(self):
         self.index = []
         self.appearances_dict = dict()
-        self.N = n
         
     def index_document(self, document: dict) -> None:
         """Process a given document and update the appearances dict."""
         terms = text_processor.get_normalized_tokens(document['title'], document['content_str'])
         for term in terms:
             if term in self.appearances_dict:
-                self.appearances_dict[term].append((document['id'], text_processor.tf(len([token for token in document['content_str'] if token == term]))))
+                self.appearances_dict[term].append((document['id'], text_processor.tf(get_tf_value(term, document['content_str']))))
             else:
-                self.appearances_dict[term] = [(document['id'], text_processor.tf(len([token for token in document['content_str'] if token == term])))]
+                self.appearances_dict[term] = [(document['id'], text_processor.tf(get_tf_value(term, document['content_str'])))]
     
     def load_index_postings_list(self) -> None:
         for term in self.appearances_dict:
@@ -32,8 +35,10 @@ class InvertedIndex:
                 'doc_tf_index': self.appearances_dict[term]
             })
 
+    @classmethod
     def get_index(self) -> List:
         return self.index
+
 
 
 def get_doc_vec_norm(term_tfs: List[float]) -> float:
@@ -43,8 +48,7 @@ def get_doc_vec_norm(term_tfs: List[float]) -> float:
     :param term_tfs: a list of term weights (log tf) for one document
     :return:
     """
-    #TODO COSINE LENGTH NORMALIZATION
-    pass
+    return (1/math.sqrt(sum(term_tfs)))
     
 
 @timer
@@ -55,15 +59,17 @@ def build_inverted_index(wapo_docs: Iterable) -> None:
         - "doc_len_index": for each doc id as a key, the value should be the "length" of that document vector
     insert the indices by using mongo_db.insert_vs_index and mongo_db.insert_doc_len_index method
     """
-    inv_ind = InvertedIndex(len(wapo_docs))
+    global N 
+    N = len(wapo_docs)
+    inv_ind = InvertedIndex()
     for doc_image in wapo_docs:
 
         ##! Weight document terms using log TF formula with cosine (length) normalization
-        insert_doc_len_index({'doc_id': doc_image['doc_id'], 'length': get_doc_vec_norm([text_processor.tf(term) for term in doc_image['content_str']])})
+        insert_doc_len_index({'doc_id': doc_image['doc_id'], 'length': get_doc_vec_norm([text_processor.tf(get_tf_value(term, doc_image['content_str'])) for term in doc_image['content_str']])})
 
         inv_ind.index_document(doc_image)
     inv_ind.load_index_postings_list()
-    # insert_db_index(sorted(inv_ind.get_index(), key = lambda i:len(i['doc_ids']), reverse=True)) # gets inserted into the db largest->smallest
+    insert_vs_index(inv_ind.get_index())
 
 
 
@@ -72,9 +78,15 @@ def parse_query(query: str) -> Tuple[List[str], List[str], List[str]]:
     helper function, should be called in query_inverted_index
     given each query, return a list of normalized terms, a list of stop words and a list of unknown words separately
     """
-    ##! Weight query terms using logarithmic TF*IDF formula without length normalization
-    pass
+    normalized_query = text_processor.get_normalized_tokens(query)
+    query_list = query.split(' ')
+    query_list = re.findall(r"[\w']+|[.,!?;]", query)
 
+    stop_words = {token for token in query_list if not text_processor.normalize(token) in normalized_query}
+    unknown_words = {token for token in query_list if isinstance(query_vs_index(text_processor.normalize(token)), type(None)) and not token in stop_words}
+
+    return (query_list, stop_words, unknown_words)
+    ##! Weight query terms using logarithmic TF*IDF formula without length normalization
 
 def top_k_docs(doc_scores: Dict[int, float], k: int) -> List[Tuple[float, int]]:
     """
@@ -84,6 +96,8 @@ def top_k_docs(doc_scores: Dict[int, float], k: int) -> List[Tuple[float, int]]:
     :param k:
     :return: a list of tuples, each tuple contains (score, doc_id)
     """
+    #! use the below to calc cos similarity of two vectors of nums
+    # result = 1 - spatial.distance.cosine(dataSetI, dataSetII)
     ##? HERE IS MY BIG QUESTION
     ##? how am I getting the param for this method, is it the doc_len_index? - where is the method for cosine similarity/length normalization supposed to go?
     pass
@@ -94,14 +108,34 @@ def query_inverted_index(query: str, k: int) -> Tuple[List[Tuple[float, int]], L
     disjunctive query over the vs_index with the help of mongo_db.query_vs_index, mongo_db.query_doc_len_index methods
     return a list of matched documents (output from the function top_k_docs), a list of stop words and a list of unknown words separately
     """
-    #call parse_query = unknown and stop
 
+    #1. get user query
+    #2. parse user query
+    #3. search index for query's tokens
+    #4. get list of postings list
+    #5. compute the tf-idf of each term in query
+    #6. iterate over the doc ids that were returned
+    #7. compute the cosine similarity (tf-idf term * doc tf) in a nested loop over each term - for each term do each doc id
+        #s7. (tf-idf*tf)/(query-length * doc-length)
+    parsed_query, stop_words, unknown_words = parse_query(query)
+    doc_scores = Dict()
+    for term in parsed_query:
+        postings_list = query_vs_index(term)['doc_tf_index']
+        for doc_tf_tuple in postings_list:
+            term_tf_idf_score = text_processor.tf(get_tf_value(term, query_doc(doc_tf_tuple[0])['content_str'])) * text_processor.idf(N, len(postings_list))
 
-    # result = 1 - spatial.distance.cosine(dataSetI, dataSetII)
+            cosine_similarity = term_tf_idf_score * doc_tf_tuple[1]
 
-    #call top_k_docs = matched documents
-    pass
+    
+def get_tf_value(term: str, content: str) -> int:
+    """ This is a method that takes a str representing an individual token and a str representing the set of tokens and calculates
+    the number of appearances of the given term in the content """
+    return len([token for token in content if token==term])
 
+def cosine_sim(tfidf_term: float, tf_doc: float, query_length: int, doc_length: float) -> float:
+    """ This is a method to get the cosine similarity between objects of given information. Namely, this method computed the cosine similarity
+    for a given tfidf score, a given tf score, and two vector lengths """
+    return (tfidf_term*tf_doc)/(query_length*doc_length)
 
 if __name__ == "__main__":
     pass
