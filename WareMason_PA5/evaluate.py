223a67a9849a9379af87c54017e542c841c55f05
masonrware <86129546+masonrware@users.noreply.github.com>
2022-03-28 19:22:47 -0400

    ready to start assignment

--- WareMason_PA5/evaluate.py
+++ WareMason_PA5/evaluate.py
@@ -1,1 +1,27 @@
+import argparse
+
+
 # TODO: add your code for evaluation
+
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        #arg1
+    )
+    parser.add_argument(
+        # "--wapo_path",
+        # required=True,
+        # type=str,
+        # help="path to the processed wapo jsonline file",
+    )
+
+    args = parser.parse_args()
+    #TODO:
+    #stuff
+    
+
+
+if __name__ == "__main__":
+    main()
