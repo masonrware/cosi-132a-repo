f3f57a603531e54c5517b619d27c49131108c64a
masonrware <86129546+masonrware@users.noreply.github.com>
2022-03-29 13:58:54 -0400

    Update evaluate.py

--- WareMason_PA5/evaluate.py
+++ WareMason_PA5/evaluate.py
@@ -1,22 +1,65 @@
 import argparse
 
 
-# TODO: add your code for evaluation
-# 
+# TODO:
+# wapo doc index has been built...
+# TODO: build out the argument tree for evaluation
+# run through each (4/5) retrieval options
+# ... get results ...
+# ...
 
 
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        #arg1
+       "--index_name",
+        metavar='{wapo_docs_50k}',
+        required=True,
+        type=str,
+        help="name of the ES index"
     )
     parser.add_argument(
-        # "--wapo_path",
-        # required=True,
-        # type=str,
-        # help="path to the processed wapo jsonline file",
+       "--topic_id",
+        required=True,
+        type=str,
+        help="topic id number"
     )
+    parser.add_argument(
+       "--query_type",
+        metavar='{kw,nl}',
+        required=True,
+        type=str,
+        help="use keyword or natural language query"
+    )
+    parser.add_argument(
+       "--search_type",
+        metavar='{vector,rerank}',
+        required=True,
+        type=str,
+        help="reranking or ranking with vector only"
+    )
+    parser.add_argument(
+       "--use_english_analyzer",
+        metavar='',
+        required=True,
+        type=str,
+        help="use english analyzer for BM25 search"
+    )
+    # parser.add_argument(
+    #    "--vector_name",   #? issue with vector in name?
+    #     metavar='{ft_vector,sbert_vector}',
+    #     required=True,
+    #     type=str,
+    #     help="use fasttext or sbert embedding"
+    # )
+    parser.add_argument(
+       "--top_k",
+        required=True,
+        type=str,
+        help="evaluate on top K ranked documents"
+    )
+    
 
     args = parser.parse_args()
     #TODO:
