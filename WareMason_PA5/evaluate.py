554f9cfdddc24eb8e658e9bb1c855f3775db8382
masonrware <86129546+masonrware@users.noreply.github.com>
2022-04-02 14:50:11 -0400

    new progress

--- WareMason_PA5/evaluate.py
+++ WareMason_PA5/evaluate.py
@@ -14,20 +14,14 @@ from typing import Any
 from example_query import generate_script_score_query, search, rescore_search
 from embedding_service.client import EmbeddingClient
 
-from elasticsearch_dsl import analyzer, tokenizer               # type: ignore
-from elasticsearch_dsl.query import MatchAll, Match             # type: ignore
+from elasticsearch_dsl.query import MatchAll, Match, Query      # type: ignore
 from elasticsearch_dsl.connections import connections           # type: ignore
 from sklearn.feature_extraction.text import TfidfVectorizer     # type: ignore
 
-## TODO
-#1. BM25, just use Search with:
-#       if ENGANA: stemmed_content
-#       else no ENGANA: content
-#2. use 
 
 class Evaluate:
     ''' A class to represent an individual evaluation run. '''
-    def __init__(self, index: str, topic: int, query_type: str, search_type: str,               # vec_name: str
+    def __init__(self, index: str, topic: int, query_type: str, search_type: str,
                  eng_ana: bool, vector_name: str, top_k: int = 20) -> None:
         self.index = index
         self.topic = topic
@@ -40,99 +34,119 @@ class Evaluate:
         self.topic_dict: dict()
         
         self.match_all_query = MatchAll()
-        self.raw_query: list()
+        self.raw_query: Any
         self.vector_query: Any
         self.basic_query: Any
         
-        
     def __str__(self) -> None:
-        print(f'{self.index}\n{self.topic}\n{self.query_type}\n{self.search_type}\n{self.eng_ana}\n{self.top_k}')
+        return (f'{self.index}\n{self.topic}\n{self.query_type}\n{self.search_type}\n{self.eng_ana}\n{self.top_k}')
         
     def process_topic(self) -> None:
         ''' Method to analyze the query and embed it. '''
-        vectorizer = TfidfVectorizer()
-        
+        # process the topic
         with open("pa5_data/pa5_queries.json", 'r') as f:
             data = json.load(f)['pa5_queries']
-        self.topic_dict = [topic_dict for topic_dict in data if int(topic_dict['topic'])==self.topic][0]
-                
+        self.topic_dict = [topic_dict for topic_dict in data if int(topic_dict['topic'])==self.topic][0] 
+    
+    def search(self) -> None:
+        ''' Method to perform searching. '''
         
-        if self.query_type=='kw':
-            X = vectorizer.fit_transform([self.topic_dict['kw']])
-            self.basic_query = Match(
-                content={"query": vectorizer.get_feature_names_out()[0]}    # get most informative word
-            )
+        # find the most informative words in query and
+        # create a basic query (one for english analyzer
+        # and one for std analyzer)
+        # also save the raw query as a str for encoding
+        vectorizer = TfidfVectorizer()
+        X = vectorizer.fit_transform([self.topic_dict[self.query_type]]) 
+        if self.eng_ana:
+            #? Better way to do this?
+            self.basic_query = Match(stemmed_content = {"query": vectorizer.get_feature_names_out()[0]})
+            self.raw_query = self.topic_dict[self.query_type]
+        else:
+            self.basic_query = Match(content = {"query": vectorizer.get_feature_names_out()[0]})
+            self.raw_query = self.topic_dict[self.query_type]
             
-            # response = my_analyzer.simulate(self.topic_dict['kw'])
-            # self.raw_query = [t.token for t in response.tokens]
-            self.raw_query = self.topic_dict['kw']
-            
-        elif self.query_type=='nl':
-            X = vectorizer.fit_transform([self.topic_dict['nl']])
-            self.basic_query = Match(
-                content = {"query": vectorizer.get_feature_names_out()[0]}
-            )
-            
-            # response = my_analyzer.simulate(self.topic_dict['nl'])
-            # self.raw_query = [t.token for t in response.tokens]
-            self.raw_query = self.topic_dict['nl']
-        
-        #! ?? HOW TO RANK (NOT RERANK) W/ SBERT? - thougt I can only rank with standard which is bm25
-        #embedding done here
+        #! HOW TO RANK W/ SBERT? - thougt I can only rank with standard bm25 (bc sbert requires embedding...)
+        #* embed with ft
         if self.vector_name == 'ft_vector':
             encoder = EmbeddingClient(host="localhost", embedding_type="fasttext")
-            self.vector_query = encoder.encode(self.raw_query, pooling="mean").tolist()[0]  # get the query embedding and convert it to a list
+            self.vector_query = encoder.encode([self.raw_query], pooling="mean").tolist()[0]  # get the query embedding and convert it to a list
             self.vector_query = generate_script_score_query(self.vector_query, "ft_vector")
+        #* embed with sbert
         elif self.vector_name == 'sbert_vector':
             encoder = EmbeddingClient(host="localhost", embedding_type="sbert")
             self.vector_query = encoder.encode([self.raw_query], pooling="mean").tolist()[0]  # get the query embedding and convert it to a list
             self.vector_query = generate_script_score_query(self.vector_query, "sbert_vector")
-
-
-    def rank_results(self) -> None:
-        ''' Method to search the db and return the specified-method-ranked results. '''
-        search("wapo_docs_50k", self.basic_query)
-        print('\n')
-        # rescore_search("wapo_docs_50k", self.basic_query, self.vector_query)
-        
-        ##??? NDCG20 ?
+            
+        if self.search_type == 'vector':
+            #* bm25 w/ either analyzer
+            rank_results(self.basic_query, self.top_k)
+            #somehow also search sbert - no way to differentiate?
+        elif self.search_type == 'rerank':
+            re_rank_results(self.basic_query, self.vector_query, self.top_k)
         
 
+
+def rank_results(query: Query, top_k: int) -> None:
+    ''' Function to search for and rank documents using the standard bm25. '''
+    search("wapo_docs_50k", query, top_k)
+    print('\n')
+    
+def re_rank_results(query: Query, vector: Query, top_k: int) -> None:
+    ''' Function to rerank documents using embeddings (fasttext and sbert). '''
+    rescore_search("wapo_docs_50k", query, vector, top_k)
+    print('\n')
+
+
+#!! NDCG20 ?
 
 
 def main():
     parser = argparse.ArgumentParser()
     connections.create_connection(hosts=["localhost"], timeout=100, alias="default")
 
+    search_type = 'vector'
+    vector_name = 'ft_vector'
     
-    parser.add_argument("--index_name", metavar='{wapo_docs_50k}', required=True,
-                        type=str, help="name of the ES index")
-    parser.add_argument("--topic_id", required=True, type=int, help="topic id number")
-    parser.add_argument("--query_type", metavar='{kw,nl}', required=True, type=str,
+    parser.add_argument("--index_name", 
+                        metavar='{wapo_docs_50k}', 
+                        required=True,
+                        type=str, 
+                        help="name of the ES index")
+    parser.add_argument("--topic_id", 
+                        required=True, 
+                        type=int, 
+                        help="topic id number")
+    parser.add_argument("--query_type", 
+                        metavar='{kw,nl}', 
+                        required=True, 
+                        type=str,
                         help="use keyword or natural language query")
-    parser.add_argument("--search_type", metavar='{vector,rerank}', required=True,
-                        type=str, help="reranking or ranking with vector only")
-    parser.add_argument("--use_english_analyzer", required=False,
-                        action='store_true', help="use english analyzer for BM25 search")
-    parser.add_argument("--vector_name", metavar='{ft_vector,sbert_vector}',
-                        required=True, type=str, help="use fasttext or sbert embedding")
-    parser.add_argument("--top_k", required=False, type=int,
+    parser.add_argument("--search_type", 
+                        metavar='{vector,rerank}', 
+                        required=False,
+                        type=str, 
+                        help="reranking or ranking with vector only")
+    parser.add_argument("--use_english_analyzer", 
+                        required=False,
+                        action='store_true', 
+                        help="use english analyzer for BM25 search")
+    parser.add_argument("--vector_name", 
+                        metavar='{ft_vector,sbert_vector}',
+                        required=False, 
+                        type=str, 
+                        help="use fasttext or sbert embedding")
+    parser.add_argument("--top_k", 
+                        required=False, 
+                        type=int,
                         help="evaluate on top K ranked documents")   
     args = parser.parse_args()
     
-    if args.use_english_analyzer:
-        eval = Evaluate(index=args.index_name, topic=args.topic_id,                                 # vec_name=args.vector_name
-                    query_type=args.query_type, search_type=args.search_type,
-                    eng_ana=True, vector_name=args.vector_name, top_k=args.top_k)
-    else:
-        eval = Evaluate(index=args.index_name, topic=args.topic_id,                                 # vec_name=args.vector_name
-                    query_type=args.query_type, search_type=args.search_type,
-                    eng_ana=False, vector_name=args.vector_name, top_k=args.top_k)
-    
-    # eval.__str__()
+    eval = Evaluate(index=args.index_name, topic=args.topic_id, query_type=args.query_type, 
+                    search_type=args.search_type if args.search_type else search_type, eng_ana=args.use_english_analyzer, 
+                    vector_name= args.vector_name if args.vector_name else vector_name, top_k=args.top_k)
+    print(eval)
     eval.process_topic()
-    eval.rank_results()
-    
+    eval.search()       #run
     
 
 if __name__ == "__main__":
