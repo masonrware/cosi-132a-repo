e26d3fecb28a0978f9f4253e98d067563d37e780
masonrware <86129546+masonrware@users.noreply.github.com>
2022-04-03 19:45:46 -0400

    Update evaluate.py

--- WareMason_PA5/evaluate.py
+++ WareMason_PA5/evaluate.py
@@ -10,7 +10,7 @@
 import argparse
 from concurrent.futures import process
 import json
-from typing import Any
+from typing import Any, Sequence
 
 from example_query import generate_script_score_query, search, rescore_search
 from embedding_service.client import EmbeddingClient
@@ -25,22 +25,23 @@ class Evaluate:
     ''' A class to represent an individual evaluation run. '''
     def __init__(self, index: str, topic: int, query_type: str, search_type: str,
                  eng_ana: bool, vector_name: str, top_k: int = 20) -> None:
-        self.index = index
-        self.topic = topic
-        self.query_type = query_type
-        self.search_type = search_type
-        self.eng_ana = eng_ana
-        self.vector_name = vector_name
-        self.top_k = top_k
+        self.index: str = index
+        self.topic: int= topic
+        self.query_type: str = query_type
+        self.search_type: str = search_type
+        self.eng_ana: bool = eng_ana
+        self.vector_name: str = vector_name
+        self.top_k: int = top_k
         
         self.topic_dict: dict()
+        self.ideal_rel_scores: Sequence[int]
         
-        self.match_all_query = MatchAll()
+        self.match_all_query: Query = MatchAll()
         self.raw_query: Any
         self.vector_query: Any
         self.basic_query: Any
         
-        ## area where I will store outputs
+        ## area where I will store outputs for params of scoring funcs
         
         
     def __str__(self) -> None:
@@ -48,10 +49,12 @@ class Evaluate:
          
     def process_topic(self) -> None:
         ''' Method to analyze the query and embed it. '''
-        # process the topic
         with open("pa5_data/pa5_queries.json", 'r') as f:
             data = json.load(f)['pa5_queries']
         self.topic_dict = [topic_dict for topic_dict in data if int(topic_dict['topic'])==self.topic][0] 
+        with open("pa5_data/ideal_relevance.json", 'r') as f:
+            data = json.load(f)
+        self.ideal_rel_scores = data[f'{self.topic}']
     
     def search(self) -> None:
         ''' Method to perform searching. '''
@@ -71,6 +74,7 @@ class Evaluate:
             self.basic_query = Match(content = {"query": ' '.join(vectorizer.get_feature_names_out()[:5])})
             self.raw_query = self.topic_dict[self.query_type]
             
+        # do embedding
         #! HOW TO RANK W/ SBERT? - thought I can only rank with standard bm25 (bc sbert requires embedding...)
         #* embed with ft
         if self.vector_name == 'ft_vector':
@@ -83,12 +87,15 @@ class Evaluate:
             self.vector_query = encoder.encode([self.raw_query], pooling="mean").tolist()[0]    # get the query embedding and convert it to a list
             self.vector_query = generate_script_score_query(self.vector_query, "sbert_vector")
             
+        # search
         if self.search_type == 'vector':
             #* bm25 w/ either analyzer
+            #! here is where I would recieve any relevance scores
             rank_results(self.basic_query, self.top_k)
             #somehow also search sbert - no way to differentiate?
         elif self.search_type == 'rerank':
             #* rerank with either embed
+            #! here is where I would recieve any relevance scores
             re_rank_results(self.basic_query, self.vector_query, self.top_k)
     
     def score(self) -> None:
@@ -119,13 +126,13 @@ class Client:
     ''' Class to run single/many evaluations of the SEO. '''
     def __init__(self, index: str, topic: int, query_type: str, search_type: str,
                  eng_ana: bool, vector_name: str, top_k: int = 20) -> None:
-        self.index = index
-        self.topic = topic
-        self.query_type = query_type
-        self.search_type = search_type
-        self.eng_ana = eng_ana
-        self.vector_name = vector_name
-        self.top_k = top_k
+        self.index: str = index
+        self.topic: int= topic
+        self.query_type: str = query_type
+        self.search_type: str = search_type
+        self.eng_ana: bool = eng_ana
+        self.vector_name: str = vector_name
+        self.top_k: int = top_k
         
     def __str__(self) -> None:
         return (f'index: {self.index}\ntopic: {self.topic}\nquery type: {self.query_type}\nsearch type: {self.search_type}\nUsing Eng. Analyzer? {self.eng_ana}\nK: {self.top_k}\n')
@@ -135,16 +142,17 @@ class Client:
         eval = Evaluate(index=self.index, topic=self.topic, query_type=self.query_type, 
                     search_type=self.search_type, eng_ana=self.eng_ana, 
                     vector_name=self.vector_name, top_k=self.top_k)
-        eval.process_topic()
-        eval.search() 
+        eval.process_topic()    # get correct topic and ideal scores
+        eval.search()           # perform search and get relevance scores
+        eval.score()            # generate a score for SEO run
 
 
 def main():
     parser = argparse.ArgumentParser()
     connections.create_connection(hosts=["localhost"], timeout=100, alias="default")
 
-    search_type = 'vector'
-    vector_name = 'ft_vector'
+    search_type: str = 'vector'
+    vector_name: str = 'ft_vector'
     
     parser.add_argument("--index_name", 
                         metavar='{wapo_docs_50k}', 
@@ -184,7 +192,7 @@ def main():
                     search_type=args.search_type if args.search_type else search_type, eng_ana=args.use_english_analyzer, 
                     vector_name= args.vector_name if args.vector_name else vector_name, top_k=args.top_k)
     
-    # run
+    # driver
     print('='*50, '\nRUNNING ELASTICSEARCH WITH THE FOLLOWING SPECS:\n', client, '='*50, '\n')
     client.run()
     
