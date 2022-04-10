c939dc9eec3c1597b15b46cb490260300c9aa6e4
masonrware <86129546+masonrware@users.noreply.github.com>
2022-04-10 13:04:14 -0400

    data upload

--- termProject/utils.py
+++ termProject/utils.py
@@ -7,19 +7,32 @@
 # Written By: Mason Ware
 
 import os
+from dotenv import load_dotenv
+import json
 import datetime
+import requests                     # type: ignore 
 
 from pynytimes import NYTAPI        # type: ignore 
 
+load_dotenv()
 
-nyt = NYTAPI('SMmx72tbIBB8gdZJgVv6gARJxjVd4o2x', parse_dates=True)
+out_file_path = 'termProject/data/movie_reviews.json'
+in_file_path = 'termProject/data/reviews.json'
+nyt = NYTAPI(os.getenv('API_KEY'), parse_dates=True)
+reviews = nyt.movie_reviews # this isn't working need to serialize
 
-reviews = nyt.movie_reviews(
-    keyword = "Green Book",
-    options = {
-        "order": "by-opening-date",
-        "reviewer": "A.O. Scott",
-        "critics_pick": False
-    })
+url = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?api-key=' + os.getenv('API_KEY')
+r = requests.get(url)
+json_data = r.json()
+json_str = json.dumps(json_data, indent=4, sort_keys=True)
 
-print(reviews)
+with open(out_file_path, 'w') as outfile:
+    outfile.write(json_str)
+    
+with open(in_file_path, 'r') as file:
+    json_data = json.load(file)
+json_str = json.dumps(json_data, indent=4, sort_keys=True)
+with open(out_file_path, 'w') as outfile:
+    outfile.write(json_str)
+
+# print(reviews)
