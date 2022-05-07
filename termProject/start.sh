e92a28c60f9ed983eaf0c88bd5f7bd49c9ef28fb
masonrware <86129546+masonrware@users.noreply.github.com>
2022-05-07 15:59:22 -0400

    added reqs

--- termProject/start.sh
+++ termProject/start.sh
@@ -8,7 +8,7 @@ python3.9 -m embedding_service.server --embedding fasttext  --model data/wiki-ne
 python3.9 -m embedding_service.server --embedding sbert  --model msmarco-distilbert-base-v3
 
 # load wapo docs into the index called "wapo_docs_50k"
-python3.9 load_es_index.py --index_name wapo_docs_50k --path data/final_unique_movie_data.json
+python3.9 load_es_index.py --index_name wapo_docs_50k --path data/final_unique_movie_data.jl
 
 # use keyword from topic 363 as the query; search over the stemmed_content field from index "wapo_docs_50k" based on BM25 and compute NDCG@20
 python3.9 evaluate.py --index_name wapo_docs_50k --topic_id 363 --query_type kw --use_english_analyzer --top_k 20
